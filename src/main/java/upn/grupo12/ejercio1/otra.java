9fbcd6b943e5d8d111422c6f26ca3bb765f9713c
sergiozegarracorne <n00399038@upn.pe>
2025-06-08 18:01:55 -0500

    sergio agrego una clase de b - otro

---
+++ src/main/java/upn/grupo12/ejercio1/otra.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package upn.grupo12.ejercio1;
+
+/**
+ *
+ * @author Sergio Z
+ */
+public class otra {
+    
+}
